8e030b349078cdcecc47e3e81019f066af14999e
threezj <27602942@qq.com>
2015-12-09 23:40:01 +0800

    check something

---
+++ public/controller/homeCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc controller
+ * @name home:homeCtrl
+ *
+ * @description
+ *
+ *
+ * @requires $scope
+ * */
+angular.module('home')
+    .controller('homeCtrl', function($scope){
+        $scope.activeMenu="Home";
+});
